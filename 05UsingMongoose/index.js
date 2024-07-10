7e985a31b2d5cdf1ec2ddc00ef47f09a69320633
doudou <doudoumao1031@gmail.com>
2024-07-10 21:04:56 +0800

    exercise01 complete

--- 05UsingMongoose/index.js
+++ 05UsingMongoose/index.js
@@ -45,11 +45,44 @@ async function createCourse() {
 
 // R
 async function getCourses() {
+    // comparison query operators
+    // eq (equal)
+    // ne (not equal)
+    // gt (greater than)
+    // gte (greater than or equal to)
+    // lt (less than)
+    // lte (less than or equal to)
+    // in
+    // nin (not in)
+    // logic operators
+    // or
+    // and
+
+    // const courses = await Course
+    //     // .find({ author: 'Mosh', isPublished: true })
+    //     // .find({ price: { $gte: 10, $lte: 20 } }) // price >= 10 && price <= 20
+    //     // .find({ price: { $in: [10, 15, 20] } }) // price == 10 || price == 15 || price == 20
+    //     // .find()
+    //     // .or([{author: 'Mosh'}, {isPublished: true}])
+    //     // .and([])
+    //     // regular expressions
+    //     .find({ author: /^Mosh/ }) // starts with Mosh
+    //     // .find({ author: /Hamedani$/i }) // ends with Hamedani, i for case insensitive
+    //     // .find({ author: /.*Mosh.*/i }) // contains Mosh
+    //     .limit(10) // limit the number of documents returned
+    //     .sort({ name: 1 }) // 1 indicates ascending order, -1 indicates descending order
+    //     // .count()
+    //     // .select({ name: 1, tags: 1 }) // select the properties you want to return
+    // console.log('courses', courses);
+
+    const pageNumber = 2;
+    const pageSize = 10;
     const courses = await Course
-        .find({ author: 'Mosh', isPublished: true })
-        .limit(10) // limit the number of documents returned
-        .sort({ name: 1 }) // 1 indicates ascending order, -1 indicates descending order
-        .select({ name: 1, tags: 1 }) // select the properties you want to return
-    console.log('courses', courses);
+        .find({author: 'Mosh', isPublished: true})
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
+        .sort({name: 1})
+        .select({name: 1, tags: 1});
+    console.log(courses)
 }
 getCourses();
